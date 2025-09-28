455c74fc2354057e604722a4f5d31623c9a25cb5
Jeffrey Zheng <zzy04212005@gmail.com>
2025-09-28 02:57:35 -0400

    Initial Commit

---
+++ Society Game/Society_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Society_GameApp.swift
+//  Society Game
+//
+//  Created by Jeffrey Zheng on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Society_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Society GameTests/Society_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  Society_GameTests.swift
+//  Society GameTests
+//
+//  Created by Jeffrey Zheng on 9/28/25.
+//
+
+import Testing
+
+struct Society_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
